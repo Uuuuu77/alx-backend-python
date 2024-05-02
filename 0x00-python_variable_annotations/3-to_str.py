03cd978b3a7b8ae71b960995e5f43fa00d7f0286
John Njuguna <johnmnjuguna89@gmail.com>
2024-05-02 13:49:02 -0700

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+""" Module for converting float to a string """
+
+
+def to_str(n: float) -> str:
+    """ Returns the string representation of the float """
+    return str(n)

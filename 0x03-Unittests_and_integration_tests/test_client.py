732331eadfd85af221713ff6f1b82c1636664da6
John Njuguna <johnmnjuguna89@gmail.com>
2024-05-27 07:17:02 -0700

    Parameterize and patch as decorators

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+""" Parameterize and patch as decorators """
+
+import unittest
+from unittest.mock import patch, MagicMock, PropertyMock
+from parameterized import parameterized
+from client import GithubOrgClient
+from typing import Dict
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ TestGithubOrgClient class """
+    @parameterized.expand([
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"}),
+    ])
+    @patch("client.get_json",)
+    def test_org(self, org: str, resp: Dict, mocked_fxn: MagicMock) -> None:
+        ''' testing GithubOrgClient response '''
+        mocked_fxn.return_value = MagicMock(return_value=resp)
+        gh_org_client = GithubOrgClient(org)
+        self.assertEqual(gh_org_client.org(), resp)
+        mocked_fxn.assert_called_once_with(
+            f"https://api.github.com/orgs/{org}"
+        )
+
+    @patch("client.GithubOrgClient.org", new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """ Test public repos url """
+        mock_org.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos"
+        }
+        self.assertEqual(
+            GithubOrgClient("google")._public_repos_url,
+            "https://api.github.com/users/google/repos",
+        )
